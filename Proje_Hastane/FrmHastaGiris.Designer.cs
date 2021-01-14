dd0dccfe6632789b7f658269fe7ca738fffaa43c
IGTCI <67021319+IGTCI@users.noreply.github.com>
2021-01-14 12:28:12 +0300

    Initial commit

---
+++ Proje_Hastane/FrmHastaGiris.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Proje_Hastane
+{
+    partial class FrmHastaGiris
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmHastaGiris));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.MskTC = new System.Windows.Forms.MaskedTextBox();
+            this.TxtSifre = new System.Windows.Forms.TextBox();
+            this.LnkUyeOl = new System.Windows.Forms.LinkLabel();
+            this.BtnGirisYap = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Script", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(79, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(335, 53);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hasta Giriş Paneli";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(132, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Şifre :";
+            // 
+            // MskTC
+            // 
+            this.MskTC.Location = new System.Drawing.Point(197, 101);
+            this.MskTC.Mask = "00000000000";
+            this.MskTC.Name = "MskTC";
+            this.MskTC.Size = new System.Drawing.Size(192, 30);
+            this.MskTC.TabIndex = 3;
+            this.MskTC.ValidatingType = typeof(int);
+            // 
+            // TxtSifre
+            // 
+            this.TxtSifre.Location = new System.Drawing.Point(197, 163);
+            this.TxtSifre.Name = "TxtSifre";
+            this.TxtSifre.PasswordChar = '*';
+            this.TxtSifre.Size = new System.Drawing.Size(192, 30);
+            this.TxtSifre.TabIndex = 4;
+            // 
+            // LnkUyeOl
+            // 
+            this.LnkUyeOl.AutoSize = true;
+            this.LnkUyeOl.Location = new System.Drawing.Point(411, 167);
+            this.LnkUyeOl.Name = "LnkUyeOl";
+            this.LnkUyeOl.Size = new System.Drawing.Size(62, 23);
+            this.LnkUyeOl.TabIndex = 5;
+            this.LnkUyeOl.TabStop = true;
+            this.LnkUyeOl.Text = "Üye Ol";
+            this.LnkUyeOl.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LnkUyeOl_LinkClicked);
+            // 
+            // BtnGirisYap
+            // 
+            this.BtnGirisYap.Location = new System.Drawing.Point(259, 217);
+            this.BtnGirisYap.Name = "BtnGirisYap";
+            this.BtnGirisYap.Size = new System.Drawing.Size(157, 40);
+            this.BtnGirisYap.TabIndex = 6;
+            this.BtnGirisYap.Text = "Giriş Yap";
+            this.BtnGirisYap.UseVisualStyleBackColor = true;
+            this.BtnGirisYap.Click += new System.EventHandler(this.BtnGirisYap_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "TC Kimlik Numara :";
+            // 
+            // FrmHastaGiris
+            // 
+            this.AcceptButton = this.BtnGirisYap;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.ClientSize = new System.Drawing.Size(498, 271);
+            this.Controls.Add(this.BtnGirisYap);
+            this.Controls.Add(this.LnkUyeOl);
+            this.Controls.Add(this.TxtSifre);
+            this.Controls.Add(this.MskTC);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Corbel", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "FrmHastaGiris";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hasta Girişi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MaskedTextBox MskTC;
+        private System.Windows.Forms.TextBox TxtSifre;
+        private System.Windows.Forms.LinkLabel LnkUyeOl;
+        private System.Windows.Forms.Button BtnGirisYap;
+        private System.Windows.Forms.Label label2;
+    }
+}
